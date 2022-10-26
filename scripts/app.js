acca80a315bfeccc84b2b4a312085827c97234fe
Muhammad Rizki Syafapri <rizkisyafapri@gmail.com>
2022-10-26 09:38:43 +0700

    finish pdf view

--- scripts/app.js
+++ scripts/app.js
@@ -6,16 +6,6 @@ const viewer = document.querySelector(".pdf-viewer");
 const search = document.getElementById("search");
 let currentPDF = {};
 
-// PWA
-if ("serviceWorker" in navigator) {
-	window.addEventListener("load", function () {
-		navigator.serviceWorker
-			.register("scripts/sw.js")
-			.then(res => console.log("service worker registered"))
-			.catch(err => console.log("service worker not registered", err))
-	})
-}
-
 function resetCurrentPDF() {
 	currentPDF = {
 		file: null,
